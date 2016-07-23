869c478feb8aa878070e03b1146c3b661242b6df
rara7886 <rara7886@gmail.com>
2016-07-22 17:00:19 -0700

    gommi

---
+++ arch/x86/include/generated/asm/.syscalls_64.h.cmd
@@ -0,0 +1,1 @@
+cmd_arch/x86/syscalls/../include/generated/asm/syscalls_64.h := /bin/bash '/home/masaki/kernel/10-136/arch/x86/syscalls/syscalltbl.sh' /home/masaki/kernel/10-136/arch/x86/syscalls/syscall_64.tbl arch/x86/syscalls/../include/generated/asm/syscalls_64.h
---
+++ arch/x86/include/generated/asm/.unistd_x32.h.cmd
@@ -0,0 +1,1 @@
+cmd_arch/x86/syscalls/../include/generated/asm/unistd_x32.h := /bin/bash '/home/masaki/kernel/10-136/arch/x86/syscalls/syscallhdr.sh' '/home/masaki/kernel/10-136/arch/x86/syscalls/syscall_64.tbl' 'arch/x86/syscalls/../include/generated/asm/unistd_x32.h' 'common,x32' '' '__X32_SYSCALL_BIT'
